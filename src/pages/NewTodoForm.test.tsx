00693a37b73a7cdf3422691292a8e2bdf1bf4d0d
John Gamboa <john@bizbox.ph>
2022-09-05 14:45:04 +0800

    added axios and make the todo buttons background transparent

--- src/pages/NewTodoForm.test.tsx
+++ src/pages/NewTodoForm.test.tsx
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import NewTodoForm from './NewTodoForm';
 
 describe('NewTodoForm', () => {
   test('render a button', async () => {
     render(<NewTodoForm />);
-    const button = await screen.findAllByRole('button', { name: 'Submit'});
+    const button = await screen.findAllByRole('button', { name: 'Submit' });
     expect(button).toHaveLength(1);
+  });
+
+  test('render a label', () => {
+    render(<NewTodoForm />);
+    const label = screen.getByText(/add new todo/i);
+    expect(label).toBeInTheDocument();
   });
 });
